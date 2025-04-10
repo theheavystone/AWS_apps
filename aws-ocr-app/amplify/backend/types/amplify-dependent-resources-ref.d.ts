52a7ae71f71c5b4625b85ee06222808244d4fda8
ZHANGLUWEI <zhangluwei@ZHANGLUWEIdeMacBook-Pro.local>
2025-04-10 16:22:21 +0900

    Lambda Setting

---
+++ aws-ocr-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ocrapp01ae7ce3fe": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "OcrFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "S3Trigger1cefa287": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "ocrImages": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
